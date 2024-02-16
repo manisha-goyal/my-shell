c430ca500c59b1299e23ec9bd13aad3f9f181876
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-02-16 00:52:57 -0500

    Milestone 2 completed

--- nyush.c
+++ nyush.c
@@ -3,17 +3,38 @@
 #include <unistd.h>
 #include <libgen.h>
 #include <limits.h>
+#include <string.h>
 
 int main() {
     char cwd[PATH_MAX]; 
+    while (1) {
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("[nyush %s]$ ", basename(cwd));
+        } else {
+            fprintf(stderr, "Error in getting base directory, getcwd()");
+            exit(-1);
+        }
+        fflush(stdout);
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("[nyush %s]$ ", basename(cwd));
-    } else {
-        fprintf(stderr, "Error in getting base directory, getcwd()");
-        exit(-1);
+        char *input = NULL;
+        size_t input_size = 0;
+        ssize_t input_chars_read;
+        input_chars_read = getline(&input, &input_size, stdin);
+
+        if (input_chars_read == -1) {
+            fprintf(stderr, "Error in getting user input, getline()");
+            free(input);
+            return 1;
+        }
+
+        printf("%s", input);
+        free(input);
     }
-
-    fflush(stdout);
-    return 0;
 }
+
+
+/*
+References
+https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
+https://opensource.com/article/22/5/safely-read-user-input-getline
+*/
