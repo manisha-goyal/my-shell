ec06754be461477dcc38cc78f40c6c78583bf41d
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-02-18 19:24:52 -0500

    Milestone 4 completed

--- nyush.c
+++ nyush.c
@@ -5,6 +5,8 @@
 #include <limits.h>
 #include <string.h>
 
+char** read_and_tokenize_input(int *num_args, char **user_input);
+
 int main(void) {
     char cwd[PATH_MAX]; 
     while (1) {
@@ -16,44 +18,72 @@ int main(void) {
         }
         fflush(stdout);
 
-        char *input = NULL;
-        size_t input_size = 0;
-        ssize_t input_chars_read;
-        input_chars_read = getline(&input, &input_size, stdin);
+        int num_args = 0;
+        char *user_input = malloc(1024);
+        char **args = read_and_tokenize_input(&num_args, &user_input);
 
-        if (input_chars_read == -1) {
-            fprintf(stderr, "Error: invalid command\n");
-            free(input);
-            exit(-1);
+        if (num_args == 0) {
+            free(args);
+            continue;
         }
-
-        input[strcspn(input, "\n")] = 0;
 
         pid_t pid = fork();
         if (pid < 0) {
-            // Fork failed
-            fprintf(stderr, "Fork failed\n");
+            fprintf(stderr, "Error: fork failed, unable to execute command\n");
             exit(-1);
         } else if (pid == 0) {
-            // Child process
-            char *args[] = {input, NULL};
-            if (execvp(input, args) == -1) {
-                fprintf(stderr, "Error: invalid command\n");
-                exit(-1);
+            if (execvp(args[0], args) == -1) {
+                fprintf(stderr, "Error: execvp failed, unable to execute command\n");
+                continue;
             }
         } else {
-            // Parent process
             int status;
             waitpid(pid, &status, 0);
         }
 
-        free(input);
+        for (int i = 0; i < num_args; i++) {
+            free(args[i]);
+        }
+        free(args);
     }
 }
 
+char** read_and_tokenize_input(int *num_args, char **user_input) {
+    char *input = NULL;
+    size_t input_size = 0;
+    ssize_t input_chars_read = getline(&input, &input_size, stdin);
+
+    if (input_chars_read == -1) {
+        fprintf(stderr, "Error: getline failed, unable to execute command\n");
+        free(input); 
+        *num_args = 0;
+        return NULL;
+    }
+
+    if (input[input_chars_read - 1] == '\n') {
+        input[input_chars_read - 1] = '\0';
+    }
+
+    strcpy(*user_input, input);
+
+    char **input_args = (char **)malloc(sizeof(char *) * (input_chars_read + 1));
+    char *saveptr;
+    *num_args = 0;
+    char *tok = strtok_r(input, " ", &saveptr);
+    while (tok != NULL) {
+        input_args[*num_args] = strdup(tok);
+        (*num_args)++;
+        tok = strtok_r(NULL, " ", &saveptr);
+    }
+
+    free(input);
+    return input_args;
+}
 
 /*
 References
 https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
 https://opensource.com/article/22/5/safely-read-user-input-getline
+https://systems-encyclopedia.cs.illinois.edu/articles/c-strtok/
+https://stackoverflow.com/questions/252782/strdup-what-does-it-do-in-c
 */
