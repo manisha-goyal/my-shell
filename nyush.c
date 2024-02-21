54e55611136d69e130224ccb4b116d27e2e80818
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-02-20 22:56:12 -0500

    Milestone 8 completed

--- nyush.c
+++ nyush.c
@@ -13,6 +13,8 @@ void memory_cleanup(char **args);
 int builtin_commands_handler(char **args, int num_args);
 void command_path_handler(char **args, char **program_path);
 int io_redirection_handler(char **args);
+int has_pipe(char **args);
+char** pipe_commands_handler(char **args, int pipe_pos, int num_args, int args_pos);
 
 int main(void) {
     signal(SIGINT, SIG_IGN);
@@ -31,6 +33,7 @@ int main(void) {
 
         int num_args = 0;
         char **args = user_input_handler(&num_args);
+        char *program_path = NULL, *program_path1 = NULL, *program_path2 = NULL;
 
         if (num_args == 0 || args == NULL) {
             if (args) 
@@ -43,45 +46,133 @@ int main(void) {
             continue;
         }
 
-        char *program_path = NULL;
-        command_path_handler(args, &program_path);
+        int pipe_pos = has_pipe(args);
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            fprintf(stderr, "Error: fork failed, unable to execute command\n");
-            free(program_path);
+        if(pipe_pos == 0) {
+            fprintf(stderr, "Error: invalid command\n");
             memory_cleanup(args);
             continue;
-        } else if (pid == 0) {
-            signal(SIGINT, SIG_DFL);
-            signal(SIGQUIT, SIG_DFL);
-            signal(SIGTSTP, SIG_DFL);
-            int io_redirection = io_redirection_handler(args);
-            if(io_redirection != 0) {
-                if (io_redirection == -1)
-                    fprintf(stderr, "Error: invalid command\n");
-                if (io_redirection == -2 || io_redirection == -3)
-                    fprintf(stderr, "Error: invalid file\n");
-                free(program_path);
-                memory_cleanup(args);
-                exit(EXIT_FAILURE);
-            }
-            if (execv(program_path, args) == -1) {
-                fprintf(stderr, "Error: invalid program\n");
-                free(program_path);
-                memory_cleanup(args);
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            int status;
-            do {
-                waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status) && !WIFSTOPPED(status));
         }
-        free(program_path);
-        memory_cleanup(args);
+
+        if(pipe_pos != -1) {
+            
+            int pipefd[2];
+            if (pipe(pipefd) == -1) {
+                fprintf(stderr, "Error: pipe failed, unable to execute command\n");
+                memory_cleanup(args);
+                continue;
+            }
+
+            char **args1 = pipe_commands_handler(args, pipe_pos, num_args, 0);
+            char **args2 = pipe_commands_handler(args, pipe_pos, num_args, 1);
+            command_path_handler(args1, &program_path1);
+            command_path_handler(args2, &program_path2);
+
+            pid_t pid1 = fork();
+            if (pid1 < 0) {
+                fprintf(stderr, "Error: fork failed, unable to execute command\n");
+                free(program_path1);
+                free(program_path2);
+                memory_cleanup(args1);
+                memory_cleanup(args2);
+                memory_cleanup(args);
+                continue;
+            }
+            if (pid1 == 0) { // First child executes the first part of the pipeline
+                close(pipefd[0]);
+                dup2(pipefd[1], STDOUT_FILENO);
+                close(pipefd[1]);
+                if (execv(program_path1, args1) == -1) {
+                    fprintf(stderr, "Error: invalid program\n");
+                    free(program_path1);
+                    free(program_path2);
+                    memory_cleanup(args1);
+                    memory_cleanup(args2);
+                    memory_cleanup(args);
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                pid_t pid2 = fork();
+                if (pid2 < 0) {
+                    fprintf(stderr, "Error: fork failed, unable to execute command\n");
+                    free(program_path1);
+                    free(program_path2);
+                    memory_cleanup(args1);
+                    memory_cleanup(args2);
+                    memory_cleanup(args);
+                    continue;
+                }
+                if (pid2 == 0) {
+                    close(pipefd[1]);
+                    dup2(pipefd[0], STDIN_FILENO);
+                    close(pipefd[0]);
+                    
+                    if (execv(program_path2, args2) == -1) {
+                        fprintf(stderr, "Error: invalid program\n");
+                        free(program_path1);
+                        free(program_path2);
+                        memory_cleanup(args1);
+                        memory_cleanup(args2);
+                        memory_cleanup(args);
+                        exit(EXIT_FAILURE);
+                    }
+                } else {
+                    close(pipefd[0]);
+                    close(pipefd[1]);
+                    int status;
+                    do {
+                        waitpid(pid1, &status, 0);
+                        waitpid(pid2, &status, 0);
+                    } while (!WIFEXITED(status) && !WIFSIGNALED(status) && !WIFSTOPPED(status));
+                    free(program_path1);
+                    free(program_path2);
+                    memory_cleanup(args1);
+                    memory_cleanup(args2);
+                    memory_cleanup(args);
+                }
+            }
+        }
+        else {
+            pid_t pid = fork();
+            if (pid < 0) {
+                fprintf(stderr, "Error: fork failed, unable to execute command\n");
+                memory_cleanup(args);
+                continue;
+            } else if (pid == 0) {
+                signal(SIGINT, SIG_DFL);
+                signal(SIGQUIT, SIG_DFL);
+                signal(SIGTSTP, SIG_DFL);
+
+                command_path_handler(args, &program_path);
+
+                int io_redirection = io_redirection_handler(args);
+                if(io_redirection != 0) {
+                    if (io_redirection == -1)
+                        fprintf(stderr, "Error: invalid command\n");
+                    if (io_redirection == -2 || io_redirection == -3)
+                        fprintf(stderr, "Error: invalid file\n");
+                    free(program_path);
+                    memory_cleanup(args);
+                    exit(EXIT_FAILURE);
+                }
+                if (execv(program_path, args) == -1) {
+                    fprintf(stderr, "Error: invalid program\n");
+                    free(program_path);
+                    memory_cleanup(args);
+                    exit(EXIT_FAILURE);
+                }
+                
+            } else {
+                int status;
+                do {
+                    waitpid(pid, &status, WUNTRACED);
+                } while (!WIFEXITED(status) && !WIFSIGNALED(status) && !WIFSTOPPED(status));
+                free(program_path);
+                memory_cleanup(args);
+            }
+        }
     }
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; 
 }
 
 void memory_cleanup(char **args) {
@@ -142,9 +233,6 @@ int builtin_commands_handler(char **args, int num_args) {
 }
 
 void command_path_handler(char **args, char **program_path) {
-    free(*program_path);
-    *program_path = NULL;
-
     char *input_path = args[0];
 
     if(input_path[0]=='/' || (input_path[0]=='.' && input_path[1]=='/')) {
@@ -228,6 +316,40 @@ int io_redirection_handler(char **args) {
     return EXIT_SUCCESS;
 }
 
+int has_pipe(char **args) {
+    int i = 0;
+    int pipe_pos = -1;
+    while (args[i] != NULL) {
+        if (strcmp(args[i], "|") == 0) {
+            pipe_pos = i;
+            return pipe_pos;
+        }
+        i++;
+    }
+    return pipe_pos;
+}
+
+char** pipe_commands_handler(char **arg, int pipe_pos, int num_args, int args_pos) {
+    if(args_pos == 0 ) {
+        char **pipe_args = malloc(sizeof(char *) * (pipe_pos + 1));
+        int i=0;
+        for(i=0; i<pipe_pos; i++) {
+            pipe_args[i] = strdup(arg[i]);
+        }
+        pipe_args[i] = NULL;
+        return pipe_args;
+    } else {
+        char **pipe_args = malloc(sizeof(char *) * (num_args - pipe_pos + 1));
+        int i=0;
+        int j=0;
+        for(i=pipe_pos + 1; i<num_args; i++) {
+            pipe_args[j++] = strdup(arg[i]);
+        }
+        pipe_args[j] = NULL;
+        return pipe_args;
+    }
+}
+
 /*
 References
 https://www.codecademy.com/resources/docs/c
@@ -242,4 +364,5 @@ https://www.scaler.com/topics/c/string-comparison-in-c/
 https://www.ibm.com/docs/en/zos/2.3.0?topic=functions-chdir-change-working-directory
 https://www.geeksforgeeks.org/concatenating-two-strings-in-c/
 https://www.cs.utexas.edu/~theksong/posts/2020-08-30-using-dup2-to-redirect-output/
+https://www.educative.io/answers/how-to-use-the-pipe-system-call-for-inter-process-communication
 */
